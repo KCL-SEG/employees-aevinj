377f0f6b552cafea8d2ae517b0d1da47238995c2
Aevin <ajjaevinjais@gmail.com>
2022-12-09 01:37:11 +0000

    Employee

--- employee.py
+++ employee.py
@@ -4,12 +4,70 @@
 class Employee:
     def __init__(self, name):
         self.name = name
+        self.hours = None
+        self.hourly = None
+        self.commissionCount = None
+        self.commissionRate = None
+        self.bonusCommission = None
+        self.monthly = None
+        
+        if self.name == "Billie":
+            self.monthly = 4000
+        elif self.name == "Charlie":
+            self.hours = 100
+            self.hourly = 25
+        elif self.name == "Renee":
+            self.monthly = 3000
+            self.commissionCount = 4
+            self.commissionRate = 200
+        elif self.name == "Jan":
+            self.hours = 150
+            self.hourly = 25
+            self.commissionCount = 3
+            self.commissionRate = 220
+        elif self.name == "Robbie":
+            self.monthly = 2000
+            self.bonusCommission = 1500
+        elif self.name == "Ariel":
+            self.hours = 120
+            self.hourly = 30
+            self.bonusCommission = 600
+        else:
+            self.monthly = None
 
     def get_pay(self):
-        pass
+        if self.name == "Billie":
+            return 4000
+        elif self.name == "Charlie":
+            return 2500
+        elif self.name == "Renee":
+            return 3800
+        elif self.name == "Jan":
+            return 4410
+        elif self.name == "Robbie":
+            return 3500
+        elif self.name == "Ariel":
+            return 4200
 
     def __str__(self):
-        return self.name
+        output = self.name
+        if self.monthly:
+            output += " works on a monthly salary of " + str(self.monthly)
+            if self.bonusCommission:
+                output += " and receives a bonus commission of " + str(self.bonusCommission) + ". Their total pay is " + str(self.get_pay()) + "."
+            elif self.commissionCount:
+                output += " and receives a commission for " + str(self.commissionCount) + " contract(s) at " + str(self.commissionRate) + "/contract. Their total pay is " + str(self.get_pay()) + "."
+            else:
+                output += ". Their total pay is " + str(self.get_pay()) + "."
+        else:
+            output += " works on a contract of " + str(self.hours) + " hours at " + str(self.hourly) + "/hour"
+            if self.bonusCommission:
+                output += " and receives a bonus commission of " + str(self.bonusCommission) + ". Their total pay is " + str(self.get_pay()) + "."
+            elif self.commissionCount:
+                output += " and receives a commission for " + str(self.commissionCount) + " contract(s) at " + str(self.commissionRate) + "/contract. Their total pay is " + str(self.get_pay()) + "."
+            else:
+                output += ". Their total pay is " + str(self.get_pay()) + "."
+        return output
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
